faaf611d6cfb436fe5bf3c3d54346771414689c7
gershancg-web <gershan.cg@gmail.com>
2026-02-24 08:10:25 +0800

    First Commit - Added AppDatabase.kt and included instances

--- app/src/main/java/com/example/smartcampuscompanion/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/database/AppDatabase.kt
@@ -1,4 +1,36 @@
 package com.example.smartcampuscompanion.data.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.smartcampuscompanion.data.dao.DepartmentDao
+import com.example.smartcampuscompanion.data.dao.UserDao
+import com.example.smartcampuscompanion.data.entity.Department
+import com.example.smartcampuscompanion.data.entity.User
+
+@Database(entities = [User::class, Department::class], version = 1, exportSchema = false)
+@TypeConverters(com.example.smartcampuscompanion.data.database.TypeConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+    abstract fun departmentDao(): DepartmentDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "smart_campus_companion_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
