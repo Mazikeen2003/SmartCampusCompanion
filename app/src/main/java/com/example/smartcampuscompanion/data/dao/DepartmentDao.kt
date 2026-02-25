04e258e8698e4add8e97f208a8ee999941b2906d
gershancg-web <gershan.cg@gmail.com>
2026-02-25 10:22:54 +0800

    SeventhCommit - Fixes on DepartmentDao.kt and UserDao.kt errors

--- app/src/main/java/com/example/smartcampuscompanion/data/dao/DepartmentDao.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/dao/DepartmentDao.kt
@@ -1,7 +1,17 @@
 package com.example.smartcampuscompanion.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.smartcampuscompanion.data.entity.Department
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DepartmentDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(departments: List<Department>)
+
+    @Query("SELECT * FROM departments")
+    fun getAll(): Flow<List<Department>>
 }
